68f5fb24f6f75b7894cc267eafd16410ecd34fa2
Ntholi Nkhatho <ntholi@live.com>
2024-11-18 15:01:07 +0200

    init database page

---
+++ src/app/database/layout.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from 'react';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { useRouter, usePathname } from 'next/navigation';
+import Container from '@/components/ui/Container';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const tabs = [
+    { value: 'rooms', label: 'Rooms', path: '/database/rooms' },
+    { value: 'classes', label: 'Classes', path: '/database/classes' },
+    { value: 'lecturers', label: 'Lecturers', path: '/database/lecturers' },
+  ];
+
+  const getCurrentTab = () => {
+    const path = pathname.split('/').pop();
+    return path || 'rooms';
+  };
+
+  return (
+    <Container className='mt-10'>
+      <Tabs
+        defaultValue={getCurrentTab()}
+        onValueChange={(value) => {
+          const tab = tabs.find((tab) => tab.value === value);
+          if (tab) router.push(tab.path);
+        }}
+      >
+        <TabsList>
+          {tabs.map((tab) => (
+            <TabsTrigger key={tab.value} value={tab.value}>
+              {tab.label}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+
+      <div className='mt-4'>{children}</div>
+    </Container>
+  );
+}
