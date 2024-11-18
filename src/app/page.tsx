bba37d58e1aa0dee20295640814f8acf85683731
Ntholi Nkhatho <ntholi@live.com>
2024-11-18 10:21:53 +0200

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className='min-h-screen p-8'>
       <h1>Hello Timetable</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
