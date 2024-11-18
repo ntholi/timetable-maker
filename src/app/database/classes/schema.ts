3918a23020ced5b2998d59700f3a9d4fb6997a45
Ntholi Nkhatho <ntholi@live.com>
2024-11-18 19:54:46 +0200

    Class name cannot contain numbers

--- src/app/database/classes/schema.ts
+++ src/app/database/classes/schema.ts
@@ -3,7 +3,11 @@ import { Faculty, faculties } from '@/entities/Faculty';
 import { Entity } from '@/entities/Entity';
 
 export const classSchema = z.object({
-  name: z.string().min(1, 'Class name is required'),
+  name: z
+    .string()
+    .min(1, 'Class name is required')
+    .max(6, 'Class name too long')
+    .regex(/^[^0-9]+$/, 'Class name cannot contain numbers'),
   faculty: z.enum(faculties as [Faculty, ...Faculty[]], {
     required_error: 'Please select a faculty',
   }),
