90a3af75e7f38f19d8da65a28c660068b42151f7
Ntholi Nkhatho <ntholi@live.com>
2024-11-23 14:28:13 +0200

    Add FBMG course data

--- src/data/courses/index.ts
+++ src/data/courses/index.ts
@@ -1,5 +1,15 @@
 import BAAS from './FABE/BAAS.json';
 import CAT from './FABE/CAT.json';
 import DAT from './FABE/DAT.json';
+import CGD from './FDI/CGD.json';
+import DCAV from './FDI/DCAV.json';
+import DGD from './FDI/DGD.json';
+import BEN from './FBMG/BEN.json';
+import BHR from './FBMG/BHR.json';
+import BIB from './FBMG/BIB.json';
+import CMK from './FBMG/CMK.json';
+import DBM from './FBMG/DBM.json';
+import DMK from './FBMG/DMK.json';
+import DRM from './FBMG/DRM.json';
 
-export { BAAS, CAT, DAT };
+export { BAAS, CAT, DAT, CGD, DCAV, DGD, BEN, BHR, BIB, CMK, DBM, DMK, DRM };
